06e109b6b49bcc74eae143d441f86cb260d79d09
soek <git@soek.de>
2017-08-21 09:57:27 +0200

    github version is productive now

--- server/install.sh
+++ server/install.sh
@@ -1,5 +1,6 @@
 npm install
-rm ../clients/soek/common_lib
-rm ../clients/ki_bot/common_lib
-ln -s  common_lib ../clients/soek/common_lib 
-ln -s  common_lib ../clients/ki_bot/common_lib
+cd ..
+rm clients/soek/common_lib
+rm clients/ki_bot/common_lib
+ln -s  ../../server/common_lib clients/soek/common_lib
+ln -s  ../../server/common_lib clients/ki_bot/common_lib
