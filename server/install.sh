b17526dbee1a48463f3fa6f60c5b3221180908c1
soek <git@soek.de>
2017-08-21 09:29:12 +0200

    bugfix in install.sh

--- server/install.sh
+++ server/install.sh
@@ -1,5 +1,5 @@
 npm install
 rm ../clients/soek/common_lib
 rm ../clients/ki_bot/common_lib
-ln -s  ../clients/soek/common_lib common_lib
-ln -s  ../clients/ki_bot/common_lib common_lib
+ln -s  common_lib ../clients/soek/common_lib 
+ln -s  common_lib ../clients/ki_bot/common_lib
