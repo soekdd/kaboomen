2721d1be1f784049c046ef106bfa645182b1fb8a
Soeren Kahl <soek@gmx.de>
2017-09-02 18:53:12 +0000

    new clients added

--- server/install.sh
+++ server/install.sh
@@ -4,3 +4,10 @@ rm clients/soek/common_lib
 rm clients/ki_bot/common_lib
 ln -s  ../../server/common_lib clients/soek/common_lib
 ln -s  ../../server/common_lib clients/ki_bot/common_lib
+ln -s  ../../server/common_lib clients/dregzo/common_lib
+ln -s  ../../server/common_lib clients/tigrix/common_lib
+ln -s  ../../server/common_lib clients/thusjab/common_lib
+ln -s  ../../server/common_lib clients/pitizzle/common_lib
+ln -s  ../../server/common_lib clients/kabooboy/common_lib
+ln -s  ../../server/common_lib clients/linus/common_lib
+ln -s  ../../server/common_lib clients/patrick/common_lib
